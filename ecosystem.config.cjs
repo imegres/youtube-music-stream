67eb8da63bee9fbf0d202d1800c9cd97ea1c631d
Jose Augusto Megres de Oliveira <49933483+augustomegres@users.noreply.github.com>
2024-08-04 21:17:33 -0300

    refactor: Remove "watch" property in ecosystem.config file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
     name: 'youtube-stream-01',
     script: 'bun',
     args: 'start',
-    watch: true,
     autorestart: true,  // Garante que o PM2 reinicie o aplicativo em caso de falha
     env: {
       NODE_ENV: 'production'
